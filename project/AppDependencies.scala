da7bdb7b67fdc06825cd98391238b3eb8c3468c0
Olushola Akinmolayan <lakinmolayan@gmail.com>
2017-07-19 17:43:25 +0100

    Changes made to codebase are stated below
    Removed git related file that should have been hidden or not checked
    into the control system
    Added scalatest and akka
    Introduced a test directory
    Update the git ignore file to just allow needed file and folders
    respectively
    Since activator template has gotten to EOL removed all folders relating to it

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -9,6 +9,8 @@ object AppDependencies {
   private val akkaHttpJson = "com.typesafe.akka" %% "akka-http-spray-json" % akkaHttpVersion
   private val akkaLogging = "com.typesafe.akka" %% "akka-slf4j" % akkaVersion
   private val logback = "ch.qos.logback" % "logback-classic" % "1.2.3"
+  private val akkatTestKit = "com.typesafe.akka" %% "akka-http-testkit" % akkaHttpVersion % Test
+  private val scalaTest = "org.scalatest" %% "scalatest" % "3.0.1" % Test
 
   val upnepaDependencies: Seq[ModuleID] =  Seq(
     akka, akkaStream, akkaHttp, akkaHttpJson, akkaLogging, logback
