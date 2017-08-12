05ff73f8ad64a18fd4d6dccdedec91fd59e68760
Muhammad Kabiru Idris <midris@swglobal.com>
2017-08-12 17:04:35 +0100

    added test specification for GPSLocationService

--- src/test/scala/com/lagos/scalameetup/upnepa/WebServerSpec.scala
+++ src/test/scala/com/lagos/scalameetup/upnepa/WebServerSpec.scala
@@ -1,8 +1,15 @@
 package com.lagos.scalameetup.upnepa
 
+import org.scalatest.FunSuite
+
 /**
  * Created by olushola on 7/19/17.
  */
-class WebServerSpec {
+class WebServerSpec extends FunSuite {
+
+
+  test("An Empty List should return true for isEmpty"){
+      assert(List().isEmpty)
+  }
 
 }
